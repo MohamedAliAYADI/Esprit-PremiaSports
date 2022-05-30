c5f33c66f30b52e4cc7f81097abbd08774dc8be2
MohamedAliAyadi <ayadi.mohamedali@esprit.tn>
2022-05-30 15:38:26 +0100

    Initialize project PremiaSports

---
+++ src/edu/esprit/tests/MainClassEvent.java
@@ -0,0 +1,14 @@
+package edu.esprit.tests;
+
+import edu.esprit.utils.MyConnection;
+
+
+
+public class MainClassEvent {
+    public static void main(String[] args) {
+
+    	MyConnection connectDB = MyConnection.getInstance();
+        
+    }
+    
+}
